b3c942269483ee16f72428a5943f465d18d323d3
Treinamento Mobile <bruno.sapienza@gmail.com>
2015-10-26 11:39:40 -0200

    Load huge img from web
    Remember to add NSAppTransportSecurity(Dictionary) -> AllowArbitraryLoads (boolean) info the info.plist

--- threads/Controller/ViewController.m
+++ threads/Controller/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+   @property (nonatomic, weak) IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -24,4 +24,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(IBAction)updateImgOnTap:(id)sender{
+    NSLog(@"logging");
+    
+    NSURL *url = [NSURL URLWithString:@"http://go.nasa.gov/1NvITOM"];
+    
+    NSData *data = [[NSData alloc] initWithContentsOfURL:url];
+    
+    UIImage *tmpImage = [[UIImage alloc] initWithData:data];
+    
+    self.imageView.image = tmpImage;
+}
 @end
